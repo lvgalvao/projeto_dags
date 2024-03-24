3939955bb4c508aa0f95982960386aadf9ab65b7
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-03-23 21:39:20 -0300

    update~ ~

--- include/db.py
+++ include/db.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://dbuser:ldxrJfmAE68oYsMKqKhcQwSeXh4Kevgc@dpg-cnv1g4ljm4es73dpa26g-a.oregon-postgres.render.com/dbname_3s0u"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLURL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
